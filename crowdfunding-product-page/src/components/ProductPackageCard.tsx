3ddbf2f80213167393d595b6fca445e4609dadc0
AuraOfDivinity <peirisasel373@gmail.com>
2026-05-15 01:31:04 +0530

    implement productPackages

---
+++ crowdfunding-product-page/src/components/ProductPackageCard.tsx
@@ -0,0 +1,49 @@
+import Button from './base/Button';
+import styles from './ProductPackageCard.module.css';
+
+type TProductPackageProps = {
+    packageName: string;
+    pledgeAmount: string;
+    description: string;
+    seatsLeft: string;
+    disabled?: boolean;
+};
+
+const ProductPackageCard = ({
+    packageName,
+    pledgeAmount,
+    description,
+    seatsLeft,
+    disabled = false,
+}: TProductPackageProps) => {
+    return (
+        <div
+            className={`${styles['productPackage__card']} ${disabled ? styles['productPackage__card--disabled'] : ''}`}
+        >
+            <div className={styles.productPackage__heading}>
+                <div className={`${styles.productPackage__title} text-preset-5-bold`}>{packageName}</div>
+                <div
+                    className={`${styles.productPackage__amount} text-preset-7-medium`}
+                >{`Pledge ${pledgeAmount} or more`}</div>
+            </div>
+            <div className={`${styles.productPackage__description} text-preset-6-regular`}>{description}</div>
+            <div className={styles.productPackage__footer}>
+                <div className={styles.productPackage__seatWrapper}>
+                    <span className={`${styles.productPackage__seatNumber} text-preset-1`}>{seatsLeft}</span>
+                    <span className={`${styles.productPackage__seatCount} text-preset-7-regular`}>left</span>
+                </div>
+                {!disabled ? (
+                    <Button color="green" size="s">
+                        Select Reward
+                    </Button>
+                ) : (
+                    <Button color="black" size="s">
+                        Out of stock
+                    </Button>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default ProductPackageCard;
---
+++ crowdfunding-product-page/src/components/base/Button.tsx
@@ -0,0 +1,30 @@
+import styles from './Button.module.css'
+
+import { ReactNode, ComponentPropsWithoutRef} from 'react'
+
+type TButtonProps = ComponentPropsWithoutRef<'button'> &{
+    color: 'green' | 'black';
+    size: 's' | 'l';
+    children? : ReactNode;
+    disabled? : boolean;
+}
+
+const Button = ({color= 'green', size ='s', children, disabled = false, ...rest}: 
+    TButtonProps) => {
+    return(
+        <button 
+        className={`
+            ${styles.button} 
+            ${disabled ? styles['button--disabled'] : styles['button--active']}
+            ${size === 's' ? styles['button--small'] : styles['button--large']}
+            ${color === 'green' ? styles['button--green'] : styles['button--black']}
+            text-preset-8-bold
+        `}
+        disabled={disabled}
+        >
+        {children}
+        </button>
+    )
+}
+
+export default Button
