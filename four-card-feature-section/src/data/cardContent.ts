cb7716db1764782f6abb581767878e14a68d9158
AuraOfDivinity <peirisasel373@gmail.com>
2026-05-21 01:45:38 +0530

    Fix image imports

--- four-card-feature-section/src/data/cardContent.ts
+++ four-card-feature-section/src/data/cardContent.ts
@@ -1,3 +1,8 @@
+import supervisorIcon from "../assets/icon-supervisor.svg";
+import teamBuilderIcon from "../assets/icon-team-builder.svg";
+import karmaIcon from "../assets/icon-karma.svg";
+import calculatorIcon from "../assets/icon-calculator.svg";
+
 export type TCardContent = {
   title: string;
   description: string;
@@ -17,26 +22,26 @@ export const baseData: TCardContentMap = {
     title: "Supervisor",
     description: "Monitors activity to identify project roadblocks",
     color: "cyan",
-    image: "/src/assets/icon-supervisor.svg",
+    image: supervisorIcon,
   },
   teamBuilder: {
     title: "Team Builder",
     description:
       "Scans our talent network to create the optimal team for your project",
     color: "red",
-    image: "/src/assets/icon-team-builder.svg",
+    image: teamBuilderIcon,
   },
   karma: {
     title: "Karma",
     description: "Regularly evaluates our talent to ensure quality",
     color: "orange",
-    image: "/src/assets/icon-karma.svg",
+    image: karmaIcon,
   },
   calculator: {
     title: "Calculator",
     description:
       "Uses data from past projects to provide better delivery estimates",
     color: "blue",
-    image: "/src/assets/icon-calculator.svg",
+    image: calculatorIcon,
   },
 };
