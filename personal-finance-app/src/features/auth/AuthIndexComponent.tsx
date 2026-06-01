125fcb93e47e6f61fea00934742bc0c6609781cb
AuraOfDivinity <peirisasel373@gmail.com>
2026-06-01 22:17:16 +0530

    Refactor AuthIndexComponent layout and styles for better responsiveness

--- personal-finance-app/src/features/auth/AuthIndexComponent.tsx
+++ personal-finance-app/src/features/auth/AuthIndexComponent.tsx
@@ -6,37 +6,42 @@ import clsx from "clsx";
 
 const AuthIndexComponent = () => {
   return (
-    <div className={styles.authIndex__wrapper}>
-      <div className={styles.authIndex__image}>
-        <img
-          className={styles.authIndex__main}
-          src={AuthSidePanelImage}
-          alt="auth side panel image"
-        />
-        <img
-          src={LogoLarge}
-          alt="logo large"
-          className={styles.authIndex__logo}
-        />
-        <div className={styles.authIndex__text}>
-          <div className={clsx(styles.authIndex__heading, "text-preset-1")}>
-            Keep track of your money and save for your future
-          </div>
-          <div
-            className={clsx(
-              styles.authIndex__subHeading,
-              "text-preset-4-regular",
-            )}
-          >
-            Personal finance app puts you in control of your spending. Track
-            transactions, set budgets, and add to savings pots easily.
+    <>
+      <div className={styles.authIndex__headerBar}>
+        <img src={LogoLarge} alt="header-logo" />
+      </div>
+      <div className={styles.authIndex__wrapper}>
+        <div className={styles.authIndex__image}>
+          <img
+            className={styles.authIndex__main}
+            src={AuthSidePanelImage}
+            alt="auth side panel image"
+          />
+          <img
+            src={LogoLarge}
+            alt="logo large"
+            className={styles.authIndex__logo}
+          />
+          <div className={styles.authIndex__text}>
+            <div className={clsx(styles.authIndex__heading, "text-preset-1")}>
+              Keep track of your money and save for your future
+            </div>
+            <div
+              className={clsx(
+                styles.authIndex__subHeading,
+                "text-preset-4-regular",
+              )}
+            >
+              Personal finance app puts you in control of your spending. Track
+              transactions, set budgets, and add to savings pots easily.
+            </div>
           </div>
         </div>
+        <div className={styles.authIndex__content}>
+          <Outlet />
+        </div>
       </div>
-      <div className={styles.authIndex__content}>
-        <Outlet />
-      </div>
-    </div>
+    </>
   );
 };
 
