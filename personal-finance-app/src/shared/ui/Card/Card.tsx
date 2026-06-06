132f36dc2e304a1efbc5a5549d85fb09b33ca155
AuraOfDivinity <peirisasel373@gmail.com>
2026-06-06 18:52:20 +0530

    Adding base Card Component

---
+++ personal-finance-app/src/shared/ui/Card/Card.tsx
@@ -0,0 +1,44 @@
+import type { ComponentPropsWithoutRef, ReactNode } from "react";
+import styles from "./Card.module.css";
+import clsx from "clsx";
+import IconCaretRight from "../../../assets/icon-caret-right.svg";
+
+type TCardProps = {
+  children: ReactNode;
+  displayTitleSection: boolean;
+  title?: string;
+  actionText?: string;
+  actionClick?: () => void;
+} & ComponentPropsWithoutRef<"div">;
+
+const Card = ({
+  title,
+  displayTitleSection = true,
+  actionText = "See Details",
+  children,
+  actionClick,
+  ...rest
+}: TCardProps) => {
+  return (
+    <div className={styles.card__wrapper} {...rest}>
+      {displayTitleSection && (
+        <div className={styles.card__title}>
+          <p className={clsx(styles.card__titleText, "text-preset-2")}>
+            {title}
+          </p>
+          <div className={styles.card__actionWrapper} onClick={actionClick}>
+            <p
+              className={clsx(styles.card__actionText, "text-preset-4-regular")}
+            >
+              {actionText}
+            </p>
+            <img src={IconCaretRight} alt="more details icon" />
+          </div>
+        </div>
+      )}
+      <div className={styles.card__content}>{children}</div>
+    </div>
+  );
+};
+
+export default Card;
